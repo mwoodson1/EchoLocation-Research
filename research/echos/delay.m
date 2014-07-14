42d4d2c82fbcd54d4bb06a3b48309032b25559dd
mwoodson1 <markus.woodson1@gmail.com>
2014-07-13 22:48:11 -0400

    more fixes

--- research/echos/delay.m
+++ research/echos/delay.m
@@ -1,13 +1,11 @@
 function [ out ] = delay( src, T, Fs )
 %delay Delays an input signal by the given time.
-%   Detailed explanation goes here
-floor(T*Fs)
+%   Simple multiplication by sampling frequency and delay by that many
+%   samples. 
 zeroo = zeros(1,floor(T*Fs));
-%Starts d secs after beggining
 echo = [zeroo src];
 
-org = [src zeroo];            %This is left in case we want to include the source signal later
-out = echo;% + org;
+out = echo;
 
 end
 
--- research/echos/generateTone.m
+++ research/echos/generateTone.m
@@ -1,45 +1,11 @@
 function [ snd, A ] = generateTone( )
-%generateTone: Given a note, time, and sampling rate
-%this function will produce a tone at that note for
-%the specified time.
-
-%Load the data into variables
-% A = load('amplitudes.mat');
-% amps = A.amplitudes;
-% octaves = A.octave;
-% 
-% %Case on the note given
-% switch f
-%     case 'E' 
-%         freq = octaves(1);
-%         A = amps(1);
-%     case 'F'
-%         freq = octaves(2);
-%         A = amps(2);
-%     case 'G'
-%         freq = octaves(4);
-%         A = amps(4);
-%     case 'A'
-%         freq = octaves(6);
-%         A = amps(6);
-%     case 'B' 
-%         freq = octaves(8);
-%         A = amps(8);
-%     case 'C'
-%         freq = octaves(9);
-%         A = amps(9);
-%     case 'D'
-%         freq = octaves(11);
-%         A = amps(11);
-% end
-% 
-% %Set the time step and produce output
-% Ts = 1/Fs;
-% t= (0:Ts:T)';
-% output = A*sin(2*pi*freq*t);
+%generateTone: 
 [r_click, Fs] = audioread('ref_click.mp3');
-snd = r_click(:,1);
+snd = r_click(2321:5163,:);
 A = max(snd);
+
+% snd = ones(1,44100*.1);
+% A = 1;
 
 end
 
